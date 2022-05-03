ce88f26678736c2989139fc35ce6aafecdbfd9dd
Hua <a47894785@gmail.com>
2022-05-03 11:47:12 +0800

    0503 Improve Login & Register Design

--- UnknownFoodDelivery/app/src/main/java/fcu/app/unknownfooddelivery/LoginActivity.java
+++ UnknownFoodDelivery/app/src/main/java/fcu/app/unknownfooddelivery/LoginActivity.java
@@ -15,6 +15,7 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
