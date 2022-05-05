8230d7e3757759b5543773666eaaf084345b8e23
sfsffk01 <93426720+sfsffk01@users.noreply.github.com>
2022-05-05 21:20:16 +0800

    0505 MainActivity_simulate UI Design

---
+++ UnknownFoodDelivery/app/src/main/java/fcu/app/unknownfooddelivery/MainActivity_simulate.java
@@ -0,0 +1,14 @@
+package fcu.app.unknownfooddelivery;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity_simulate extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_simulate);
+    }
+}
