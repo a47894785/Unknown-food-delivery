bb934713d5a66a9a415f10967b49cbe68c92a8df
ulayhe <93426442+ulayhe@users.noreply.github.com>
2022-05-23 20:54:58 +0800

    0523 temp

--- UnknownFoodDelivery/app/src/main/java/fcu/app/unknownfooddelivery/HomeFragment.java
+++ UnknownFoodDelivery/app/src/main/java/fcu/app/unknownfooddelivery/HomeFragment.java
@@ -1,5 +1,6 @@
 package fcu.app.unknownfooddelivery;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,15 +8,21 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationCallback;
 import com.google.android.gms.location.LocationRequest;
 
+import java.util.ArrayList;
+
 public class HomeFragment extends Fragment {
 
   TextView tvCurrentAddress;
+  /*public static final String ALBUM_NO = "album_no";
 
   @Override
   public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -26,11 +33,25 @@ public class HomeFragment extends Fragment {
 
     tvCurrentAddress = view.findViewById(R.id.home_frag_address);
 
+
+    ListView lvAlbum = getActivity().findViewById(R.id.imageView17);
+
+    ArrayList<String> albumList = new ArrayList<String>();
+
+    /*albumList.add("台北之旅");
+    albumList.add("台中之旅");
+    albumList.add("高雄之旅");*/
+
+    /*ArrayAdapter<String> adapter = new ArrayAdapter(getActivity(), android.R.layout.simple_list_item_1, albumList);
+
+    lvAlbum.setAdapter(adapter);
+
+
     if (getArguments() != null) {
       String currentAddress = this.getArguments().getString("address");
       tvCurrentAddress.setText(currentAddress);
     }
 
-    return view;
+    return view;*/
   }
 }
