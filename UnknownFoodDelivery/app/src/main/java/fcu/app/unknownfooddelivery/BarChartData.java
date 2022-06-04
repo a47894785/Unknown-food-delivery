2deab4b31d4dc48bb3652b18646bf48b7cb160c6
sfsffk01 <93426720+sfsffk01@users.noreply.github.com>
2022-06-04 20:25:34 +0800

    0604 BarChart

---
+++ UnknownFoodDelivery/app/src/main/java/fcu/app/unknownfooddelivery/BarChartData.java
@@ -0,0 +1,112 @@
+package fcu.app.unknownfooddelivery;
+
+import android.content.Context;
+import android.graphics.Color;
+import android.util.Log;
+
+import androidx.core.content.ContextCompat;
+
+import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.components.AxisBase;
+import com.github.mikephil.charting.components.Description;
+import com.github.mikephil.charting.components.Legend;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.components.YAxis;
+import com.github.mikephil.charting.data.BarData;
+import com.github.mikephil.charting.data.BarDataSet;
+import com.github.mikephil.charting.data.BarEntry;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
+import com.github.mikephil.charting.formatter.IAxisValueFormatter;
+import com.github.mikephil.charting.formatter.IndexAxisValueFormatter;
+import com.github.mikephil.charting.formatter.ValueFormatter;
+
+import java.text.DecimalFormat;
+import java.util.ArrayList;
+
+public class BarChartData {
+  Context context;
+  BarChart barChart;
+
+  public BarChartData(BarChart barChart, Context context){
+    this.context = context;
+    this.barChart = barChart;
+  }
+
+  public void initDataSet(ArrayList<BarEntry> valuesY) {
+    if(valuesY.size()>0){
+      for(int i = 0 ;i<valuesY.size();i++){
+        final BarDataSet set;
+        set = new BarDataSet(valuesY, "");
+        //set.setMode(BarDataSet.Mode.LINEAR);//類型為折線
+        set.setColor(ContextCompat.getColor(context, R.color.main_green));//線的顏色
+        //set.setCircleColor(ContextCompat.getColor(context, R.color.black));//圓點顏色
+        //set.setCircleRadius(4);//圓點大小
+        //set.setDrawCircleHole(false);//圓點為實心(預設空心)
+        //set.setLineWidth(1.5f);//線寬
+        set.setDrawValues(true);//顯示座標點對應Y軸的數字(預設顯示)
+        set.setValueTextSize(8);//座標點數字大小
+
+        Legend legend = barChart.getLegend();
+        legend.setEnabled(false);//不顯示圖例 (預設顯示)
+        Description description = barChart.getDescription();
+        description.setEnabled(false);//不顯示Description Label (預設顯示)
+
+        BarData data = new BarData(set);
+        barChart.setData(data);//一定要放在最後
+      }
+    }else{
+      barChart.setNoDataText("暫時沒有數據");
+      barChart.setNoDataTextColor(Color.BLUE);//文字顏色
+    }
+    barChart.invalidate();//繪製圖表
+  }
+
+  public void initX(ArrayList dateList) {
+    Log.d("LineChart", String.valueOf(barChart));
+    XAxis xAxis = barChart.getXAxis();
+
+    xAxis.setPosition(XAxis.XAxisPosition.BOTTOM);//X軸標籤顯示位置(預設顯示在上方，分為上方內/外側、下方內/外側及上下同時顯示)
+    xAxis.setTextColor(Color.GRAY);//X軸標籤顏色
+    xAxis.setTextSize(12);//X軸標籤大小
+
+    xAxis.setLabelCount(dateList.size());//X軸標籤個數
+    xAxis.setSpaceMin(0.5f);//折線起點距離左側Y軸距離
+    xAxis.setSpaceMax(0.5f);//折線終點距離右側Y軸距離
+
+    xAxis.setDrawGridLines(false);//不顯示每個座標點對應X軸的線 (預設顯示)
+
+    xAxis.setValueFormatter(new IndexAxisValueFormatter(dateList));
+  }
+
+  public void initY(Float min, Float max) {
+    YAxis rightAxis = barChart.getAxisRight();//獲取右側的軸線
+    rightAxis.setEnabled(false);//不顯示右側Y軸
+    YAxis leftAxis = barChart.getAxisLeft();//獲取左側的軸線
+
+    leftAxis.setLabelCount(4);//Y軸標籤個數
+    leftAxis.setTextColor(Color.GRAY);//Y軸標籤顏色
+    leftAxis.setTextSize(12);//Y軸標籤大小
+
+    leftAxis.setAxisMinimum(min-10);//Y軸標籤最小值
+    leftAxis.setAxisMaximum(max+10);//Y軸標籤最大值
+
+    leftAxis.setValueFormatter(new MyYAxisValueFormatter());
+  }
+
+  class MyYAxisValueFormatter extends ValueFormatter {
+
+    private DecimalFormat mFormat;
+
+    public MyYAxisValueFormatter() {
+      mFormat = new DecimalFormat("###,###.0");//Y軸數值格式及小數點位數
+    }
+
+    @Override
+    public String getFormattedValue(float value, AxisBase axis) {
+      return mFormat.format(value);
+    }
+  }
+}
--- UnknownFoodDelivery/app/src/main/java/fcu/app/unknownfooddelivery/ShopHistoryFragment.java
+++ UnknownFoodDelivery/app/src/main/java/fcu/app/unknownfooddelivery/ShopHistoryFragment.java
@@ -1,50 +1,35 @@
 package fcu.app.unknownfooddelivery;
 
-import android.content.Context;
-import android.graphics.Color;
 import android.os.Bundle;
 
-import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ListView;
 
-import com.github.mikephil.charting.charts.LineChart;
-import com.github.mikephil.charting.components.AxisBase;
-import com.github.mikephil.charting.components.Description;
-import com.github.mikephil.charting.components.Legend;
-import com.github.mikephil.charting.components.XAxis;
-import com.github.mikephil.charting.components.YAxis;
-import com.github.mikephil.charting.data.Entry;
-import com.github.mikephil.charting.data.LineData;
-import com.github.mikephil.charting.data.LineDataSet;
-import com.github.mikephil.charting.formatter.IAxisValueFormatter;
-import com.github.mikephil.charting.formatter.IndexAxisValueFormatter;
+import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.data.BarEntry;
 
-import java.text.DecimalFormat;
 import java.util.ArrayList;
-import java.util.List;
 
 public class ShopHistoryFragment extends Fragment {
-  LineChartData lineChartData;
-  LineChart lineChart;
+  BarChartData lineChartData;
+  BarChart lineChart;
   ArrayList<String> xData = new ArrayList<>();
-  ArrayList<Entry> yData = new ArrayList<>();
+  ArrayList<BarEntry> yData = new ArrayList<>();
 
   @Override
   public View onCreateView(LayoutInflater inflater, ViewGroup container,
                            Bundle savedInstanceState) {
 
     View view = inflater.inflate(R.layout.fragment_shop_history, container, false);
-    lineChart = (LineChart) view.findViewById(R.id.line_chart);
-    lineChartData = new LineChartData(lineChart, getActivity());
+    lineChart = (BarChart) view.findViewById(R.id.bar_chart);
+    lineChartData = new BarChartData(lineChart, getActivity());
 
     for(int i = 0; i < 10; i++){
       xData.add("第" + i + "筆");
-      yData.add(new Entry(i-1, i));
+      yData.add(new BarEntry(i-1, i));
     }
     lineChartData.initX(xData);
     lineChartData.initY(0F,10F);
